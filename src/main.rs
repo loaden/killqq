e8f06be02ac0f215c7a9520059b95b20b6c297be
Yuchen Deng <loaden@gmail.com>
2022-05-31 07:07:37 +0800

    学习函数调用

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::fs;
 
-fn kill_qq() {
-    for number in 1..=5 {
-        println!("{}!", number);
-    }
-}
-
 fn main() {
     println!("Hello, world!");
     let contents = fs::read_to_string("in.txt").expect("in.txt does not exist");
-    println!("{}", contents);
-    kill_qq();
+    let r = kill_qq(&contents);
+    println!("{}, {}", contents.len(), r);
+}
+
+fn kill_qq(contents: &String) -> String {
+    println!("{}!", contents.len());
+    let ret = String::from("return");
+    ret
 }
