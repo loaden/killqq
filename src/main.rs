7af7b1b11ac4e922e3bac88eeb3151ac159dd6ae
Yuchen Deng <loaden@gmail.com>
2022-05-31 16:05:01 +0800

    添加undo

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,18 @@ fn main() {
     let contents = fs::read_to_string("in.txt").expect("in.txt does not exist");
     let mut rules = fs::read_to_string("rules.txt").expect("rules.txt does not exist");
     kill_qq(contents, &mut rules);
-    fs::write("rules.txt", rules.as_str());
+    fs::write("rules.txt", rules.as_str()).expect("write rules.txt failed");
+    
+    let mut undo = String::new();
+    let mut cnt = 0;
+    loop {
+        undo.push_str(format!("undo {}\n", cnt).as_str());
+        cnt += 10;
+        if cnt > 2000 {
+            fs::write("undo.txt", undo.as_str()).expect("write undo.txt failed");
+            break;
+        }
+    }
 }
 
 fn kill_qq(contents: String, rules: &mut String) -> usize {
@@ -15,7 +26,6 @@ fn kill_qq(contents: String, rules: &mut String) -> usize {
     )
     .unwrap();
     for line in contents.lines() {
-        println!("LINE: {}", line);
         for cap in re.captures_iter(line) {
             if ! &cap[1].eq("10") && rules.find(&cap[0]).is_none() {
                 rules.push_str(format!("network host address {}\n", &cap[0]).as_str());
