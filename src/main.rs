872435862237bc6ed3c07bb53ca3477cfe7748c2
Yuchen Deng <loaden@gmail.com>
2022-05-31 11:32:51 +0800

    正则表达式获取地址

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use regex::Regex;
 
 fn main() {
     println!("Hello, world!");
@@ -9,12 +10,14 @@ fn main() {
 
 fn kill_qq(contents: &String) -> String {
     println!("{}!", contents.len());
+    let re = Regex::new(r"((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}").unwrap();
     for line in contents.lines() {
         println!("LINE: {}", line);
-        for piece in line.split("-") {
-            println!("SPLIT: {}", piece);
+        for cap in re.captures_iter(line) {
+            println!("RET: {}", &cap[0]);
         }
     }
+
     let ret = String::from("return");
     ret
 }
