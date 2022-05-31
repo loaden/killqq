ea2c7ba86a181eef03efadd2ff221fc857543874
Yuchen Deng <loaden@gmail.com>
2022-05-31 14:49:22 +0800

    实现规则自动生成

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
     let contents = fs::read_to_string("in.txt").expect("in.txt does not exist");
-    let r = kill_qq(&contents);
-    println!("{}, {}", contents.len(), r);
+    let mut rules = fs::read_to_string("rules.txt").expect("rules.txt does not exist");
+    kill_qq(contents, &mut rules);
+    fs::write("rules.txt", rules.as_str());
 }
 
-fn kill_qq(contents: &String) -> String {
+fn kill_qq(contents: String, rules: &mut String) -> usize {
     println!("{}!", contents.len());
-    let re = Regex::new(r"((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}").unwrap();
+    let re = Regex::new(
+        r"((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}",
+    )
+    .unwrap();
     for line in contents.lines() {
         println!("LINE: {}", line);
         for cap in re.captures_iter(line) {
-            println!("RET: {}", &cap[0]);
+            if ! &cap[1].eq("10") && rules.find(&cap[0]).is_none() {
+                rules.push_str(format!("network host address {}\n", &cap[0]).as_str());
+                println!("RET: {}", &cap[0]);
+            }
         }
     }
 
-    let ret = String::from("return");
-    ret
+    println!("{}", rules);
+    rules.len()
 }
